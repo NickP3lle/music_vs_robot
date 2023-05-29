569e520edbb44cb5bedf906803ff21a19bf9d509
P3lle <nicolop3lle@gmail.com>
2023-05-29 11:43:56 +0200

    Added comments and reorganised the code

--- src/music/music_instrument.h
+++ src/music/music_instrument.h
@@ -15,22 +15,25 @@ class MusicInstruments : public Entity {
     u32 level;
 
   public:
-    static u32 damages[ROWS][3];
     MusicInstruments(u32 life, u32 damage, u32 level = 0);
     virtual MusicInstruments *clone() const override = 0;
+
+    static u32 damages[ROWS][3];
+
     bool takeDamage(u32 &damage) override;
-    u32 attack() const override final;
     virtual void attack(u32 row) const = 0;
-    virtual u32 getCost() const = 0;
     virtual u32 value() const = 0;
+    u32 attack() const override final;
     virtual ostream &print(ostream &os) const;
     virtual void levelUp();
-    u32 getLevel() const;
     static void resetDamages();
+
+    u32 getLevel() const;
+    virtual u32 getCost() const = 0;
 };
 
 /**
- * Classe che rappresenta gli strumenti musicali
+ * Classi che rappresentano gli strumenti musicali
  */
 
 /** Flute */
@@ -38,6 +41,7 @@ class Flute : public MusicInstruments {
   public:
     Flute(u32 health = FLUTE_DEFAULT_HEALTH, u32 power = FLUTE_DEFAULT_POWER, u32 level = 0);
     Flute *clone() const override;
+
     virtual void attack(u32 row) const override;
     void levelUp() override;
     u32 getCost() const override { return FLUTE_UPDATE_PRICE * 0.75; }
@@ -50,6 +54,7 @@ class Drum : public MusicInstruments {
   public:
     Drum(u32 health = DRUM_DEFAULT_HEALTH, u32 power = DRUM_DEFAULT_POWER, u32 level = 0);
     Drum *clone() const override;
+
     void attack(u32 row) const override;
     void levelUp() override;
     u32 getCost() const override { return DRUM_UPDATE_PRICE * 0.75; }
@@ -66,6 +71,7 @@ class Saxophone : public MusicInstruments {
     Saxophone(u32 health = SAXOPHONE_DEFAULT_HEALTH, u32 power = SAXOPHONE_DEFAULT_POWER, u32 level = 0,
               bool secondLife = true);
     Saxophone *clone() const override;
+
     void attack(u32 row) const override;
     void levelUp() override;
     u32 getCost() const override { return SAXOPHONE_UPDATE_PRICE * 0.75; }
@@ -79,6 +85,7 @@ class Trumpet : public MusicInstruments {
   public:
     Trumpet(u32 health = TRUMPET_DEFAULT_HEALTH, u32 power = TRUMPET_DEFAULT_POWER, u32 level = 0);
     Trumpet *clone() const override;
+
     void attack(u32 row) const override;
     void levelUp() override;
     u32 value() const override;
@@ -91,6 +98,7 @@ class Violin : public MusicInstruments {
   public:
     Violin(u32 health = VIOLIN_DEFAULT_HEALTH, u32 power = VIOLIN_DEFAULT_POWER, u32 level = 0);
     Violin *clone() const override;
+
     void attack(u32 row) const override;
     void levelUp() override;
     u32 value() const override;
