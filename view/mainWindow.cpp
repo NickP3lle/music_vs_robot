1645a2cf0fff1777e1dda6bd8444cc0900ef0287
P3lle <nicolop3lle@gmail.com>
2023-06-05 18:05:26 +0200

    StartWidget modified so that it stay in center position and adjusted sizes for now. Must be created the gameplay or playground window and connected to the new and load game button

--- view/mainWindow.cpp
+++ view/mainWindow.cpp
@@ -1,14 +1,13 @@
 #include "mainWindow.h"
 
 #include "startWidget.h"
+#include <QLabel>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("Music vs Robots");
     StartWidget *startWidget = new StartWidget(this);
     setCentralWidget(startWidget);
+    // startWidget->setStyleSheet("background-color:black;");
     startWidget->resize(1024, 576);
-    startWidget->setStyleSheet("background-color:black;");
-    setCentralWidget(startWidget);
-    // centralWidget()->setLayout(new QVBoxLayout(this));
-    // centralWidget()->layout()->addWidget(new QLabel("Main Window", this));
 }
--- view/startWidget.cpp
+++ view/startWidget.cpp
@@ -1,7 +1,10 @@
 #include "startWidget.h"
 
+#include <QApplication>
 #include <QLabel>
 #include <QPushButton>
+#include <QScreen>
+#include <QSpacerItem>
 #include <QVBoxLayout>
 
 StartWidget::StartWidget(QWidget *parent) : QWidget(parent) {
@@ -10,10 +13,36 @@ StartWidget::StartWidget(QWidget *parent) : QWidget(parent) {
     QPushButton *loadButton = new QPushButton("Load Game", this);
     QPushButton *quitButton = new QPushButton("Quit", this);
 
+    QSpacerItem *topSpacer = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Expanding);
+    QSpacerItem *bottomSpacer = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    title->setStyleSheet("color: white; font-size: 70px; font-weight: "
+                         "bold; text-align: center;");
+    title->setAlignment(Qt::AlignCenter);
+    // title->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    startButton->setStyleSheet("padding: 10px; color: white; font-size: 30px;");
+    loadButton->setStyleSheet("padding: 10px; color: white; font-size: 30px;");
+    quitButton->setStyleSheet("padding: 10px; color: white; font-size: 30px;");
+
+    title->setFixedSize(600, 150);
+    startButton->setFixedSize(220, 50);
+    loadButton->setFixedSize(220, 50);
+    quitButton->setFixedSize(220, 50);
+
+    connect(quitButton, SIGNAL(clicked()), parent, SLOT(close()));
+
     QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addItem(topSpacer);
     layout->addWidget(title);
     layout->addWidget(startButton);
     layout->addWidget(loadButton);
     layout->addWidget(quitButton);
+    layout->addItem(bottomSpacer);
+
+    layout->setAlignment(title, Qt::AlignCenter);
+    layout->setAlignment(startButton, Qt::AlignCenter);
+    layout->setAlignment(loadButton, Qt::AlignCenter);
+    layout->setAlignment(quitButton, Qt::AlignCenter);
+
     setLayout(layout);
 }
