7afa2933504e2da2b128dd1c0983acfcba2a4b77
danesinoo <carlorosso02@gmail.com>
2023-08-19 12:29:44 +0200

    updated picture, uploaded missing ones, updated resources.qrc and imageVisitor.* accordingly

--- view/visitors/imageVisitor.cpp
+++ view/visitors/imageVisitor.cpp
@@ -1,15 +1,63 @@
 #include "ImageVisitor.h"
 
-void ImageVisitor::visitSlowDown() const { pixmap = new QPixmap(":/assets/violin.png"); }
+void ImageVisitor::visitDamageBullet() const {
+  pixmap = new QPixmap(":/assets/DamageBullet.png");
+}
 
-void ImageVisitor::visitThreeRow() const { pixmap = new QPixmap(":/assets/trumpet.png"); }
+void ImageVisitor::visitDamageSlow() const {
+  pixmap = new QPixmap(":/assets/DamageSlow.png");
+}
 
-void ImageVisitor::visitThreeColumn() const { pixmap = new QPixmap(":/assets/drum.png"); }
+void ImageVisitor::visitDamageWave() const {
+  pixmap = new QPixmap(":/assets/DamageWave.png");
+}
 
-void ImageVisitor::visitSample() const { pixmap = new QPixmap(":/assets/flute.png"); }
+void ImageVisitor::visitSlowDown() const {
+  pixmap = new QPixmap(":/assets/Violin.png");
+}
 
-void ImageVisitor::visitDoubleLife() const { pixmap = new QPixmap(":/assets/saxophone.png"); }
+void ImageVisitor::visitThreeRow() const {
+  pixmap = new QPixmap(":/assets/Trumpet.png");
+}
 
-void ImageVisitor::visitEnemy() const { pixmap = new QPixmap(":/assets/robot.png"); }
+void ImageVisitor::visitThreeColumn() const {
+  pixmap = new QPixmap(":/assets/Drum.png");
+}
+
+void ImageVisitor::visitSample() const {
+  pixmap = new QPixmap(":/assets/Flute.png");
+}
+
+void ImageVisitor::visitDoubleLife() const {
+  pixmap = new QPixmap(":/assets/Saxophone.png");
+}
+
+void ImageVisitor::visitEnemy() const {
+  pixmap = new QPixmap(":/assets/Robot.png");
+}
+
+void ImageVisitor::visitEnemyDefense() const {
+  pixmap = new QPixmap(":/assets/RobotDefense.png");
+}
+
+void ImageVisitor::visitEnemyBig() const {
+  pixmap = new QPixmap(":/assets/RobotBig.png");
+}
+
+void ImageVisitor::visitToolWeapon() const {
+  pixmap = new QPixmap(":/assets/ToolWeapon.png");
+}
+
+void ImageVisitor::visitToolArmor() const {
+  pixmap = new QPixmap(":/assets/ToolArmor.png");
+}
+
+void ImageVisitor::visitToolBoots() const {
+  pixmap = new QPixmap(":/assets/ToolBoots.png");
+}
+
+void ImageVisitor::visitToolRing() const {
+  pixmap = new QPixmap(":/assets/ToolRing.png");
+}
 
 QPixmap *ImageVisitor::getPixmap() const { return pixmap; }
--- view/visitors/imageVisitor.h
+++ view/visitors/imageVisitor.h
@@ -1,37 +1,36 @@
 #pragma once
 
-#include "../../logic/util/visitor.h"
+#include "../../v2/util/visitor.h"
 
 #include <QPixmap>
 
 class ImageVisitor : public VisitorInterface {
-  private:
-    QPixmap *pixmap;
+private:
+  QPixmap *pixmap;
 
-  public:
-    /// Damage
-    void visitDamageBullet() const override{};
-    void visitDamageSlow() const override{};
-    void visitDamageWave() const override{};
-    void visitDamageEnemy() const override{};
+public:
+  /// Damage
+  void visitDamageBullet() const override;
+  void visitDamageSlow() const override;
+  void visitDamageWave() const override;
 
-    /// Player
-    void visitSample() const override;
-    void visitThreeColumn() const override;
-    void visitDoubleLife() const override;
-    void visitThreeRow() const override;
-    void visitSlowDown() const override;
+  /// Player
+  void visitSample() const override;
+  void visitThreeColumn() const override;
+  void visitDoubleLife() const override;
+  void visitThreeRow() const override;
+  void visitSlowDown() const override;
 
-    /// Robot
-    void visitEnemy() const override;
-    void visitEnemyDefense() const override;
-    void visitEnemyBig() const override;
+  /// Robot
+  void visitEnemy() const override;
+  void visitEnemyDefense() const override;
+  void visitEnemyBig() const override;
 
-    /// Tool
-    void visitToolWeapon() const override{};
-    void visitToolArmor() const override{};
-    void visitToolBoots() const override{};
-    void visitToolRing() const override{};
+  /// Tool
+  void visitToolWeapon() const override;
+  void visitToolArmor() const override;
+  void visitToolBoots() const override;
+  void visitToolRing() const override;
 
-    QPixmap *getPixmap() const;
+  QPixmap *getPixmap() const;
 };
